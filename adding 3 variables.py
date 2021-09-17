b34f6cb3374c1a4f44afead0779e300ee9bb63ea
Ellen Sim <ellensimjitong@gmail.com>
2021-09-18 01:11:31 +0800

    Print name function added

---
+++ adding 3 variables.py
@@ -0,0 +1,17 @@
+a=5
+b=7
+c=9
+sum=(a+b+c)
+print("a=", a, "b=" , b, "c=", c, "Sum=", sum)
+
+name1 = 'ELLEN'
+# classmate's name
+names = ['Ellen', 'Alex','Joe', 'Ryan']
+
+# loop through all names
+for i in names:
+    # print it out
+    print(i)
+
+
+print("finished")
