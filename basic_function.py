861855c434561468e810413827e774cd8a685e96
Ellen Sim <ellensimjitong@gmail.com>
2021-09-24 11:50:51 +0800

    commit on 'main'

---
+++ basic_function.py
@@ -0,0 +1,22 @@
+print("FUNCTION 1")
+print("Ellen")
+print("Alex")
+print("Ryan")
+print("Joe")
+
+a = 5
+b = 7
+c = 9
+
+print("FUNCTION 2")
+print(a + b + c)
+
+print("FUNCTION 3")
+print(a*a)
+
+print("FUNCTION 4")
+
+d = 10
+e = 20
+if e>d:
+    print("e is larger than d")
