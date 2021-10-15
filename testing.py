9566357203e4a9dd8b907e745beaed102d65a979
Ellen Sim <ellensimjitong@gmail.com>
2021-10-15 10:44:00 +0800

    commit

---
+++ testing.py
@@ -0,0 +1,21 @@
+import constraint
+problem = constraint.Problem()
+
+problem.addVariable('x', [1,2,3])
+problem.addVariable('y', range(10))
+
+def our_constraint(x, y):
+    if x + y >= 5:
+        return True
+
+problem.addConstraint(our_constraint, ['x','y'])
+
+solutions = problem.getSolutions()
+length = len(solutions)
+print("(x,y) ∈ {", end="")
+for index, solution in enumerate(solutions):
+    if index == length - 1:
+        print("({},{})".format(solution['x'], solution['y']), end="")
+    else:
+        print("({},{}),".format(solution['x'], solution['y']), end="")
+print("}")
